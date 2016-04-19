235127a1a7dd17dc56844c05439e8625a1bba1a7
Ivan Brdar <ivan.brdar1@skole.hr>
2016-04-19 17:29:16 +0200

    Spjanaje na bazu podataka

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php require ('db.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Automobili</title>
+</head>
+
+<body>
+<h1>
+    Automobili
+</h1>
+
+<p>
+    <?php
+    $sql = "SELECT id, registracija, ime FROM automobili";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        echo('<table>');
+        while($row = $result->fetch_assoc()) {
+            echo('<tr>');
+            //echo "id: " . $row["id"]. " - registracija: " . $row["registracija"]. " " . $row["ime"]. "<br>";
+            echo('<td>' . $row['id'] . '</td>');
+            echo('<td>' . $row['registracija'] . '</td>');
+            echo('<td>' . $row['ime'] . '</td>');
+            echo('<td><img src="delete_icon.jpg"></td> ');
+            echo('</tr>');
+        }
+        echo('</table>');
+    } else {
+        echo "Nema automobila";
+    }
+    $conn->close();
+    ?>
+</p>
+<p>
+    <a href="unos.php">Unos novog automobila</a>
+</p>
+</body>
+</html>
